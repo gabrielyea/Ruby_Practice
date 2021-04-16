332cfb1846b10d7f92f66cff0a5fc692a7357481
Gabriel <gabriel.gabrielyea@gmail.com>
2021-04-16 17:24:21 -0500

    bubble sort finish

---
+++ Ruby-BubbleSort/BubbleSort.rb
@@ -0,0 +1,40 @@
+arr = [4,3,78,2,0,2]
+
+
+translate = -> (i) {
+    temp = arr[i]
+    arr[i] = arr[i + 1]
+    arr[i + 1] = temp
+}
+
+
+
+def print_space()
+
+end
+
+def bubble_sort(arr, translate)
+    i = 0;
+    limit = arr.size - 2
+    checkArray = -> {print arr; puts}
+    needSort = -> (a, b) {a > b}
+    startOver = -> {
+            print "pass ----------------- pass"
+            puts
+            limit -= 1
+            i = 0
+    }
+  
+    checkArray.()
+
+    until limit === 0 do 
+        if(needSort.(arr[i], arr[i + 1]))
+            translate.(i)
+            checkArray.()
+        end
+        i += 1      
+        startOver.() if i > limit
+    end  
+end
+
+bubble_sort(arr, translate)
